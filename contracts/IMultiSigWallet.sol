83ac7605f9b8ad500252963bae1649af074bbafb
Steven Enamakel <enamakel@mahadao.com>
2022-10-20 22:22:15 +0100

    formatted contracts

--- contracts/IMultiSigWallet.sol
+++ contracts/IMultiSigWallet.sol
@@ -2,109 +2,58 @@
 pragma solidity ^0.8.4;
 
 interface IMultiSigWallet {
-    event Confirmation(address indexed sender, uint indexed transactionId);
-    event Revocation(address indexed sender, uint indexed transactionId);
-    event Submission(uint indexed transactionId);
-    event Execution(uint indexed transactionId);
-    event ExecutionFailure(uint indexed transactionId);
-    event Deposit(address indexed sender, uint value);
+    event Confirmation(address indexed sender, uint256 indexed transactionId);
+    event Revocation(address indexed sender, uint256 indexed transactionId);
+    event Submission(uint256 indexed transactionId);
+    event Execution(uint256 indexed transactionId);
+    event ExecutionFailure(uint256 indexed transactionId);
+    event Deposit(address indexed sender, uint256 value);
     event OwnerAddition(address indexed owner);
     event OwnerRemoval(address indexed owner);
-    event RequirementChange(uint required);
+    event RequirementChange(uint256 required);
 
+    function addOwner(address owner) external;
 
-    /// @dev Allows to add a new owner. Transaction has to be sent by wallet.
-    /// @param owner Address of new owner.
-    function addOwner(address owner)
-        external;
+    function isOwner(address owner) external returns (bool);
 
-    function isOwner(address owner)
-        external returns (bool);
+    function removeOwner(address owner) external;
 
-    /// @dev Allows to remove an owner. Transaction has to be sent by wallet.
-    /// @param owner Address of owner.
-    function removeOwner(address owner)
-        external;
+    function replaceOwner(address owner, address newOwner) external;
 
-    /// @dev Allows to replace an owner with a new owner. Transaction has to be sent by wallet.
-    /// @param owner Address of owner to be replaced.
-    /// @param newOwner Address of new owner.
-    function replaceOwner(address owner, address newOwner)
-        external;
+    function changeRequirement(uint256 _required) external;
 
-    /// @dev Allows to change the number of required confirmations. Transaction has to be sent by wallet.
-    /// @param _required Number of required confirmations.
-    function changeRequirement(uint _required)
-        external;
+    function submitTransaction(
+        address destination,
+        uint256 value,
+        bytes memory data
+    ) external;
 
-    /// @dev Allows an owner to submit and confirm a transaction.
-    /// @param destination Transaction target address.
-    /// @param value Transaction ether value.
-    /// @param data Transaction data payload.
-    function submitTransaction(address destination, uint value, bytes memory data)
-        external;
+    function confirmTransaction(uint256 transactionId) external;
 
-    /// @dev Allows an owner to confirm a transaction.
-    /// @param transactionId Transaction ID.
-    function confirmTransaction(uint transactionId)
-        external;
+    function revokeConfirmation(uint256 transactionId) external;
 
-    /// @dev Allows an owner to revoke a confirmation for a transaction.
-    /// @param transactionId Transaction ID.
-    function revokeConfirmation(uint transactionId)
-        external;
+    function executeTransaction(uint256 transactionId) external;
 
-    /// @dev Allows anyone to execute a confirmed transaction.
-    /// @param transactionId Transaction ID.
-    function executeTransaction(uint transactionId)
-        external;
+    function isConfirmed(uint256 transactionId) external view returns (bool);
 
-    /// @dev Returns the confirmation status of a transaction.
-    /// @param transactionId Transaction ID.
-    /// @return Confirmation status.
-    function isConfirmed(uint transactionId)
+    function getConfirmationCount(uint256 transactionId)
         external
         view
-        returns (bool);
+        returns (uint256 count);
 
-    /*
-     * Web3 call functions
-     */
-    /// @dev Returns number of confirmations of a transaction.
-    /// @param transactionId Transaction ID.
-    function getConfirmationCount(uint transactionId)
-        external
-        view
-        returns (uint count);
+    function getTransactionCount(bool pending, bool executed) external view;
 
-    /// @dev Returns total number of transactions after filers are applied.
-    /// @param pending Include pending transactions.
-    /// @param executed Include executed transactions.
-    function getTransactionCount(bool pending, bool executed)
-        external
-        view;
+    function getOwners() external view returns (address[] memory);
 
-    /// @dev Returns list of owners.
-    /// @return List of owner addresses.
-    function getOwners()
-        external
-        view
-        returns (address[] memory);
-
-    /// @dev Returns array with owner addresses, which confirmed transaction.
-    /// @param transactionId Transaction ID.
-    function getConfirmations(uint transactionId)
+    function getConfirmations(uint256 transactionId)
         external
         view
         returns (address[] memory _confirmations);
 
-    /// @dev Returns list of transaction IDs in defined range.
-    /// @param from Index start position of transaction array.
-    /// @param to Index end position of transaction array.
-    /// @param pending Include pending transactions.
-    /// @param executed Include executed transactions.
-    function getTransactionIds(uint from, uint to, bool pending, bool executed)
-        external
-        view
-        returns (uint[] memory _transactionIds);
+    function getTransactionIds(
+        uint256 from,
+        uint256 to,
+        bool pending,
+        bool executed
+    ) external view returns (uint256[] memory _transactionIds);
 }
--- contracts/StablecoinImpl.sol
+++ contracts/StablecoinImpl.sol
@@ -2,51 +2,46 @@
 pragma solidity ^0.8.4;
 
 import {ERC20, EIP712, ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import {IMultiSigWallet} from './IMultiSigWallet.sol';
+import {IMultiSigWallet} from "./IMultiSigWallet.sol";
 
-/// @title Multisignature wallet - Allows multiple parties to agree on transactions before execution.
-/// @author Stefan George - <stefan.george@consensys.net>
 contract StablecoinImpl is ERC20Permit {
     IMultiSigWallet multiSigWallet;
-    mapping(address => bool)  isFrozen;
+    mapping(address => bool) isFrozen;
 
     event FrozenStatus(address indexed from, address indexed who, bool status);
 
-    constructor(string memory _name, string memory _symbol, address _wallet) ERC20(_name, _symbol) ERC20Permit(_name) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _wallet
+    ) ERC20(_name, _symbol) ERC20Permit(_name) {
         multiSigWallet = IMultiSigWallet(_wallet);
     }
 
-
-
-    modifier onlyMultisigOwner () {
+    modifier onlyMultisigOwner() {
         require(multiSigWallet.isOwner(msg.sender), "only multisig owner");
         _;
     }
 
-    modifier onlyMultisig () {
+    modifier onlyMultisig() {
         require(address(multiSigWallet) == msg.sender, "only multisig");
         _;
     }
 
-
-
-    function mint(address _who, uint256 amount) onlyMultisig external {
+    function mint(address _who, uint256 amount) external onlyMultisig {
         _mint(_who, amount);
     }
 
-
-    function burn(address _who, uint256 amount) onlyMultisig  external {
+    function burn(address _who, uint256 amount) external onlyMultisig {
         _burn(_who, amount);
     }
 
-
-    function freeze(address _who) onlyMultisigOwner external {
+    function freeze(address _who) external onlyMultisigOwner {
         isFrozen[_who] = true;
         emit FrozenStatus(msg.sender, _who, true);
     }
 
-
-    function unfreeze(address _who) onlyMultisigOwner external  {
+    function unfreeze(address _who) external onlyMultisigOwner {
         isFrozen[_who] = false;
         emit FrozenStatus(msg.sender, _who, false);
     }
